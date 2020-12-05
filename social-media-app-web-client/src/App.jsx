d4e23614de480568d0fd8c4e64518bbd169ee652
casserekevin <kevin_cassere13@hotmail.com>
2020-12-05 20:53:35 -0300

    Getting Started With React

---
+++ social-media-app-web-client/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+//pages
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
+
+//components
+import Navbar from './components/Navbar'
+
+//css
+import './global.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <Router>
+                    <Navbar/>
+                    <div className="container">
+                        <Switch>
+                            <Route exact path='/' component={home}/>
+                            <Route exact path='/login' component={login}/>
+                            <Route exact path='/signup' component={signup}/>
+                        </Switch>
+                    </div>
+                </Router>
+            </div>
+        )
+    }
+}
+
+export default App
+
