a3499315b0a88942cf4f8e4be2741ecd423ab8df
casserekevin <kevin_cassere13@hotmail.com>
2020-12-06 15:40:37 -0300

    Signup and Auth State

--- social-media-app-web-client/src/pages/login.jsx
+++ social-media-app-web-client/src/pages/login.jsx
@@ -14,32 +14,9 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 //image imports
 import ApeImage from '../images/icon.png'
 
-const styles = {
-    form: {
-        textAlign: 'center',
-    },
-    image: {
-        margin: '20px auto 20px auto'
-    },
-    pageTitle: {
-        margin: '10px auto 10px auto'
-    },
-    textField: {
-        margin: '10px auto 10px auto'
-    },
-    button: {
-        marginTop: 20,
-        position: 'relative'
-    },
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: 10
-    },
-    progress: {
-        position: 'absolute'
-    }
-}
+const styles = (theme) => ({
+    ...theme.spreadThis
+})
 
 
 class login extends Component {
@@ -64,7 +41,7 @@ class login extends Component {
         }
         axios.post('/login', userData)
         .then((result) => {
-            console.log(result.data);
+            localStorage.setItem('FirebaseAuthenticationToken', `Bearer ${result.data.token}`)
             this.setState({
                 loading: false
             })
--- social-media-app-web-client/src/pages/signup.jsx
+++ social-media-app-web-client/src/pages/signup.jsx
@@ -1,13 +1,108 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import PropTypes from 'prop-types'
+
+//material UI imports
+import withStyles from '@material-ui/core/styles/withStyles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+//image imports
+import ApeImage from '../images/icon.png'
+
+const styles = (theme) => ({
+    ...theme.spreadThis
+})
+
 
 class signup extends Component {
+    constructor(){
+        super()
+        this.state = {
+            email: '',
+            password: '',
+            confirmPassword: '',
+            handle: '',
+            loading: false,
+            errors: {}
+        }
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.setState({
+            loading: true
+        })
+        const newUserData = {
+            email: this.state.email,
+            password: this.state.password,
+            confirmPassword: this.state.confirmPassword,
+            handle: this.state.handle
+        }
+        axios.post('/signup', newUserData)
+        .then((result) => {
+            localStorage.setItem('FirebaseAuthenticationToken', `Bearer ${result.data.token}`)
+            this.setState({
+                loading: false
+            })
+            this.props.history.push('/')
+        })
+        .catch((error) => {
+            this.setState({
+                errors: error.response.data,
+                loading: false
+            })
+        })
+
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+
     render() {
+        const { classes } = this.props
+        const { loading, errors } = this.state
         return (
-            <div>
-                <h1>signup page</h1>
-            </div>
+            <Grid container className={classes.form}>
+                <Grid item sm/>
+                <Grid item sm>
+                    <img src={ApeImage} alt='monkey' className={classes.image}/>
+                    <Typography variant="h2" className={classes.pageTitle}>Sign up</Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+                        <TextField id='email' name='email' type='email' label='Email' fullWidth helperText={errors.email} error={errors.email ? true : false} value={this.state.email} onChange={this.handleChange} className={classes.textField}/>
+                        <TextField id='password' name='password' type='password' label='Password' fullWidth helperText={errors.password} error={errors.password ? true : false} value={this.state.password} onChange={this.handleChange} className={classes.textField}/>
+                        <TextField id='confirmPassword' name='confirmPassword' type='password' label='Confirm Password' fullWidth helperText={errors.confirmPassword} error={errors.confirmPassword ? true : false} value={this.state.confirmPassword} onChange={this.handleChange} className={classes.textField}/>
+                        <TextField id='handle' name='handle' type='text' label='Handle' fullWidth helperText={errors.handle} error={errors.handle ? true : false} value={this.state.handle} onChange={this.handleChange} className={classes.textField}/>
+                        {errors.general && (
+                            <Typography variant='body2' className={classes.customError}>
+                                {errors.general}
+                            </Typography>
+                        )}
+                        <Button type='submit' variant='contained' color='primary' disabled={loading} className={classes.button}>
+                            Sign up
+                            {loading && (
+                                <CircularProgress size={30} className={classes.progress}/>
+                            )}
+                        </Button> <br/>
+                        <small>Already have an account? <Link to='/login'>Login</Link></small>
+                    </form>
+                </Grid>
+                <Grid item sm/>
+            </Grid>
         )
     }
 }
 
-export default signup
+signup.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(signup)
