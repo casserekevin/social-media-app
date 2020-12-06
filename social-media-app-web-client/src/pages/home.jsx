afc6d6b95654ebb805fe24920d462a54150742e0
casserekevin <kevin_cassere13@hotmail.com>
2020-12-06 00:31:32 -0300

    Post Card Details

--- social-media-app-web-client/src/pages/home.jsx
+++ social-media-app-web-client/src/pages/home.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+import Grid from '@material-ui/core/Grid'
+
+import Scream from '../components/Scream'
 
 class Home extends Component {
+    state = {
+        screams: null
+    }
+
+    componentDidMount(){
+        axios.get('/screams')
+        .then((result) => {
+            console.log(result.data)
+            this.setState({
+                screams: result.data
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
     render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map((scream) => <Scream scream={scream}/>)
+        ) : (
+            <p>Loading...</p>
+        )
+
         return (
-            <div>
-                <h1>Home page</h1>
-            </div>
+            <Grid container spacing={2}>
+                <Grid item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>profile...</p>
+                </Grid>
+            </Grid>
         )
     }
 }
